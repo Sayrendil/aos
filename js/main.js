6bb4376fda57286de632a817b8234e4417068879
Daniyar <daniyarsigaev@mail.ru>
2021-05-19 23:54:29 +0600

    Two Commit

--- js/main.js
+++ js/main.js
@@ -272,5 +272,13 @@
 		bgVideo();
 	});
 
+	var $page = $('html, body');
+	$('a[href*="#"]').click(function() {
+	    $page.animate({
+	        scrollTop: $($.attr(this, 'href')).offset().top
+	    }, 2000);
+	    return false;
+	});
+
 
 }());
